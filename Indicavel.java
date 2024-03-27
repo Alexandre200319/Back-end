d4b2da6cb82902ff0b96aab2b67b1867b3ab0f07
Alexandre Hermes <133255531+Alexandre200319@users.noreply.github.com>
2024-03-27 12:46:28 -0300

    Delete Indicavel.java

--- Indicavel.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public interface Indicavel {
-    Boolean isElegivel();
-    Short getNumeroIndicacoes();
-    void setElegivel(Boolean elegivel);
-    void setNumeroIndicacoes(Short numeroIndicacoes);
-}
